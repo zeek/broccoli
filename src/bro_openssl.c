959cc0a8181e7f4b07559a6aecca2a0d7d3d445c
Robin Sommer <robin@icir.org>
2015-11-10 13:30:03 -0800

    Fix to compile with OpenSSL that has SSLv3 disalbed.
    Patch by Christoph Pietsch.

--- src/bro_openssl.c
+++ src/bro_openssl.c
@@ -302,7 +302,7 @@ __bro_openssl_init(void)
    * to set up an SSL connection now and abort if this fails in any way.
    */
 
-  if (! (ctx = SSL_CTX_new(SSLv3_method())))
+  if (! (ctx = SSL_CTX_new(SSLv23_method())))
     D_RETURN_(FALSE);
   
   /* We expect things to be stored in PEM format, which means that we
